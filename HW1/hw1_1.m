f6d29521bebdba482e006c4620eef61fae82a48f
ChihMin <s102062111@m102.nthu.edu.tw>
2015-03-22 22:23:44 +0800

    add PSNR Calculate

--- HW1/hw1_1.m
+++ HW1/hw1_1.m
@@ -1,8 +1,19 @@
 img = imread('data/BFCatvengers.png');
 
+subplot(1, 2, 1);
+imshow(img);
+
 [h, w, ~] = size( img );
 img = im2double( img );
 img = Demosaicing( img );
 fprintf('%d %d\n', h, w);
+
+subplot( 1, 2, 2);
 imshow(img);
 imwrite(img, 'demosaicing_output.png', 'png');
+
+origImg = imread('data/Catvengers.png');
+distImg = imread('demosaicing_output.png');
+PSNR = CalculatePSNR(origImg, distImg);
+
+fprintf('PSNR is %f %f %f\n', PSNR(1), PSNR(2), PSNR(3) );
