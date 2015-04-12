e73630ccb53a9939ef1d6f03c6a2640c48ed8e90
ChihMin <s102062111@m102.nthu.edu.tw>
2015-04-12 23:43:13 +0800

    complete homwork #2 part 1

--- HW2/Part1/hw2_part1.m
+++ HW2/Part1/hw2_part1.m
@@ -17,19 +17,23 @@ xlabel (promptStr, 'FontName', '·L³n¥¿¶ÂÅé', 'FontSize', 14);
 outlineVertexList = ctrlPointList; %Enrich outlineVertexList
 [pt_counts, ~] = size(outlineVertexList);
 
-i = 0;
 M = [-1 3 -3 1; 3 -6 3 0; -3 3 0 0; 1 0 0 0];
 G = zeros(4, 2);
-for i = 1:4,
-    for j = 1:2,
-        G(i, j) = ctrlPointList(i, j);
-    end
-end
 syms t;
 sample = linspace(0, 1, 40);
 ansList = zeros(0, 2, 'double');
 [~, sample_counts] = size(sample);
 
+pt_counts 
+
+cur_pt = 0;
+while( cur_pt < pt_counts )
+    for i = 1:4,
+        for j = 1:2,
+            G(i, j) = ctrlPointList(mod((cur_pt + i - 1),pt_counts) + 1, j);
+        end
+    end
+    
     for i = 1:sample_counts,
         t = sym(sample(i));
         T = [t.^3, t.^2, t.^1, 1];
@@ -37,10 +41,22 @@ ansList = zeros(0, 2, 'double');
         ans = double(vpa(ans));
         ansList = [ansList; ans];
     end
+    cur_pt = cur_pt + 3;
+end
 
 outlineVertexList = ansList;
-% ctrlPointList = ansList;
+ctrlPointList = ansList;
 
 %% Draw and fill the polygon
 drawAndFillPolygon( catImage, ctrlPointList, outlineVertexList, true, true, true ); %ctrlPointScattered, polygonPlotted, filled
 
+%{
+    big_img = imresize(catImage, 4, 'nearest');
+    imwrite(big_img, 'github_cat_4_scale.png', 'png');
+    figure;
+    % output 4 multiple scaling of image 
+
+    ctrlPointList = ctrlPointList * 4;
+    outlineVertexList = outlineVertexList * 4;
+    drawAndFillPolygon( big_img, ctrlPointList, outlineVertexList, true, true, true ); %ctrlPointScattered, polygonPlotted, filled
+%}
