750c49341796b5cf5f9db049836b0265b8ca3532
ChihMin <s102062111@m102.nthu.edu.tw>
2015-05-11 10:40:19 +0800

    add window function, but have some bugs

---
+++ HW3/hw3.m
@@ -0,0 +1,27 @@
+%% Q1
+
+% hw3.m - 請完整寫出把三首歌都分出來的過程
+clear all;close all;clc;
+
+% Read in input audio file (wavread or audioread)
+[y, Fs, nbits] = wavread('hw3_mix.wav');
+[Ans, fltr] = myFilter(y, Fs, nbits, 'Hanning', 'low-pass', [0.1 0.1]);
+audiowrite('low_pass.wav', Ans, Fs);
+sound(Ans, Fs);
+fvtool(fltr);
+% Filtering
+
+% Frequency analysis - you can use the following code to plot spectrum
+% y1: signal, Fs1: sampling rate
+y1 = fltr;
+Fs1 = Fs;
+L = 2^nextpow2(max(size(y1)));
+y1_FFT = fft(y1,L);
+xx = Fs1/2*linspace(0,1,L/2+1);
+figure, plot(xx,abs(y1_FFT(1:L/2+1)));
+
+
+
+% Save the filtered audio (wavwrite or audiowrite)
+
+%%
---
+++ HW3/myFilter.m
@@ -0,0 +1,72 @@
+function [out filter] = myFilter(y, Fs, nbits, wFun, type, para)
+
+% in: input signal
+% fs: sampling frequency
+% N : size of FIR filter, assumed to be odd
+% wFun: 'Hanning', 'Hamming', 'Blackman'
+% type: 'low-pass', 'high-pass', 'bandpass', 'bandstop' 
+% para: cut-off frequency or band frequencies corresponding to the filter type
+%       if type is 'low-pass' or 'high-pass', para has only one element         
+%       if type is 'bandpass' or 'bandstop', para is a vector of 2 elements
+
+% 1. Normalization
+
+
+% 2. Create the filter according the ideal equations in Table5.2
+ans = [];
+fltr = [];
+if strcmp(type,'low-pass')==1, 
+    f_c = para(1);
+    time = [1:length(y)] / Fs;
+    f_c = 0.8;
+    W_c = 2 * pi * f_c;
+
+    N = 501;
+    middle = floor(N/2);
+    
+    for n = (-middle):middle,
+        if n == 0
+            fltr(middle+1) = 1;
+        else
+            fltr(n+middle+1) = sin(2 * pi * f_c * n) / (pi * n);
+        end
+    end
+    
+   
+    fltr(middle) = 2 * f_c;
+    subplot(1, 3, 1);
+    plot([1:length(fltr)], fltr); 
+    
+    for n = 1:N,
+        fltr(n) = fltr(n) * (0.5 + 0.5*cos((2*pi*(n-1))/(N)));    
+    end
+    
+    
+    subplot(1, 3, 2);
+    plot([1:length(fltr)], fltr); 
+    fltr = fir1(N, 0.2);
+    subplot(1, 3, 3);
+    plot([1:length(fltr)], fltr);
+    
+    ans = conv(y, fltr);
+    
+    time = [1:length(fltr)] / Fs;
+    a = max(ans);
+    b = abs(min(ans));
+    
+    average = sum(ans) / length(ans);
+    scale = 1;
+    ans = ans * scale;
+end
+
+out = ans;
+filter = fltr;
+
+% 3. Create the windowing function
+if strcmp(wFun,'Hanning')==1,
+end
+
+
+% 4. Get the realistic filter
+
+% 5. Filter the input signal in time domain. Do not use matlab function 'conv'
