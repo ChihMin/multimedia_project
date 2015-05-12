aede7b9fac5bc9c1403e5587bd369adf87ebfb26
ChihMin <s102062111@m102.nthu.edu.tw>
2015-05-12 13:25:18 +0800

    complete HW3

--- HW3/hw3.m
+++ HW3/hw3.m
@@ -19,15 +19,6 @@ fvtool(fltr);
 
 % Frequency analysis - you can use the following code to plot spectrum
 % y1: signal, Fs1: sampling rate
-%{
-y1 = y;
-Fs1 = Fs;
-L = 2^nextpow2(max(size(y1)));
-y1_FFT = fft(y1,L);
-xx = Fs1/2*linspace(0,1,L/2+1);
-figure, plot(xx,abs(y1_FFT(1:L/2+1)));
-%}
-
 
 % Save the filtered audio (wavwrite or audiowrite)
 
@@ -41,39 +32,49 @@ subplot(2, 2, 1);
 plot_sound(y, Fs);
 title('origin sound');
 
-% Add random noise to sound & plot
-[h, ~] = size(y);
-noise = y + rand(h, 2);
 
-subplot(2, 2, 2);
-plot_sound(noise, Fs);
-audiowrite('AnJing_4bit_noise.wav', noise, Fs);
+
 
 % first-order feedback loop for noise shaping
-noise = noise * (2^nbits/2)+(2^nbits/2);
-F_in = y*(2^nbits/2)+(2^nbits/2);
-F_out = y*(2^nbits/2)+(2^nbits/2);
-c = 0.9;
+nbits = 8;
+F_in =  floor(y*(2^nbits/2)+(2^nbits/2));
+F_out = floor(y*(2^nbits/2)+(2^nbits/2));
+c = 0.8;
+
+% Add random noise to sound & plot
+[h, ~] = size(y);
+s = 1;
+
+noise = F_in/s + -1 + 2 * rand(1);
+subplot(2, 2, 2);
+plot_sound(noise, Fs);
+
 for j = 1:2,
-    for i = 2:h,
-        Ei = F_in(i-1, j) - F_out(i-1, j);
-        F_out(i, j) = floor(noise(i, j) + c * Ei);
+    for i = 1:h,
+        if i == 1,
+            Ei = 0;
+            F_out(i, j) = floor(noise(i, j) + c * Ei);
+        else
+            Ei = F_in(i-1, j)/s - F_out(i-1, j);
+            F_out(i, j) = floor(noise(i, j) + c * Ei);
+   
+        end
     end
 end
 
-F_out = (F_out - (2^nbits/2))/ (2^nbits/2);
-
+F_out =(F_out - (2^nbits/2))/(2^nbits/2);
 subplot(2, 2, 3);
 plot_sound(F_out, Fs);
 audiowrite('AnJing_4bit_feedback.wav', F_out, Fs);
-% sound(F_out, Fs);
+
 
 % low_pass filter
-[low_pass(:,1), fltr] = myFilter(y(:, 1), Fs, nbits, 'Hanning', 'low-pass', 500);
-[low_pass(:,2), fltr] = myFilter(y(:, 2), Fs, nbits, 'Hanning', 'low-pass', 500);
+[low_pass(:,1), fltr] = myFilter(F_out(:, 1), Fs, nbits, 'Hanning', 'low-pass', 500);
+[low_pass(:,2), fltr] = myFilter(F_out(:, 2), Fs, nbits, 'Hanning', 'low-pass', 500);
 low_pass = low_pass * 2;
 audiowrite('AnJing_4bit_low_pass.wav', low_pass, Fs);
 subplot(2, 2, 4);
 plot_sound(low_pass, Fs);
+
 % sound(low_pass, Fs);
 
